e96863caaa2090dd37eb898d875ba777cb9df0ee
GabyArchila <87708508+GabyArchila@users.noreply.github.com>
2021-09-30 11:53:28 -0600

    Update Receta.java

--- Nutry-Safe/src/Receta.java
+++ Nutry-Safe/src/Receta.java
@@ -1,4 +1,14 @@
 public class Receta {
+    public Receta(){
+    }
+    public String recetaCalorias(){
+        if (u.getCaloria_objetivo <= 2400 && u.getCaloria_objetivo >= 2000) {
+            return "Desayuno: \n\t-1 taza de café cpn leche desnatda. \n\t-2 tostadas integrales con mermelada de frutas. \nMerienda: \n\t-1 yogurt desntado. \n\t-5 nueces. \n\t-1 plátano en rodajas. \nAlmuerzo: \n\t-Macarrones con brócoli. \n\t-1 pechuga de pollo a la plancha. \n\t-1 manzana. \nCena: \n\t-Merluza ala plancha. \n\t-1 taza de puré de calabaza. \n\t-1 infusión de lo que desee. ";
+        }
+        else if (u.getCaloria_objetivo <= 2700 && u.getCaloria_objetivo >= 2500){
+             return "Desayuno: \n\t-250g tortilla francesa. \n\t-100g de avena. \n\t-1 vaso de leche. \n\t-10 almendras. \nMerienda: \n\t-150g de pavo. \n\t-2 rebanadas de pan integral. \n\t-1 manzana. \nAlmuerzo: \n\t-2 latas de atún. \n\t-2 rebanadas de pan integral. \n\t-1 naranja. \nCena: \n\t-100g de salmón. \n\t-150g de ensalda con agucate y con 15g de aceite de oliva.";
+         }
+    }
     
     public static String darConsejo(Usuario U){
         if(U.getEdad() >=19 && U.getEdad() <= 25) {

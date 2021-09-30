5a703f61e772cb2f4b836108bcaf81cc3cf12748
SeongjunAn <an20224@uvg.edu.gt>
2021-09-30 06:02:17 -0600

    Update Receta.java

--- Nutry-Safe/src/Receta.java
+++ Nutry-Safe/src/Receta.java
@@ -1,3 +1,36 @@
 public class Receta {
     
+    Usuario usuario = new Usuario();
+
+    public void darReceta(Usuario U){
+        if(U.getEdad >=19 && U.getEdad <= 25) {
+            System.out.println("La caloria necesaria para un joven es aproximadamente 2900 calorias. La caloria que usted desea consumir es" + " " + U.getCaloria_objetivo() + ".");
+        }
+
+
+        else if(U.getEdad >=26 && U.getEdad <= 45) {
+            System.out.println("La caloria necesaria para un adulto es aproximadamente 2600 calorias. La caloria que usted desea consumir es" + " " + U.getCaloria_objetivo() + ".");
+        }
+        
+        else if(U.getEdad >=46 && U.getEdad <= 65) {
+            System.out.println("La caloria necesaria para un adulto mayor es aproximadamente 2400 calorias. La caloria que usted desea consumir es" + " " + U.getCaloria_objetivo() + ".");
+        }
+        
+        else if(U.getEdad >=66) {
+            System.out.println("La caloria necesaria para un gente de tercer edad es aproximadamente 2200 calorias. La caloria que usted desea consumir es" + " " + U.getCaloria_objetivo() + ".");
+        }
+    
+        else if(U.getEdad >=10 && U.getEdad <= 18) {
+            System.out.println("La caloria necesaria para un niño es aproximadamente 2000 calorias. La caloria que usted desea consumir es" + " " + U.getCaloria_objetivo() + ".");
+        }
+    
+    }
+    
+
+    
+    
+
+
+
+
 }

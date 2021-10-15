d4d963c8485c1a4a043ed89f52c18fcbf46f2830
ManuelAlejandroMartinezFlores <manuelalejandromartinezf@gmail.com>
2021-10-15 09:13:40 -0600

    implementar menu

--- Nutry_Safe/src/PanelGeneral.java
+++ Nutry_Safe/src/PanelGeneral.java
@@ -34,11 +34,30 @@ public class PanelGeneral extends JPanel {
 		consejos_p.setVisible(false);
 		add(consejos_p);
 		
+		PanelReceta receta_p = new PanelReceta();
+		receta_p.setBounds(151, 0, 349, 273);
+		receta_p.setVisible(false);
+		add(receta_p);
+		
+		PanelCalorias calorias_p = new PanelCalorias();
+		calorias_p.setBounds(151, 0, 349, 273);
+		calorias_p.setVisible(false);
+		add(calorias_p);
+		
 		JLabel opciones_l = new JLabel("Opciones");
 		opciones_l.setBounds(22, 21, 61, 16);
 		add(opciones_l);
 		
 		JButton recetas_b = new JButton("Recetas");
+		recetas_b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				consejos_p.setVisible(false);
+				datos_p.setVisible(false);
+				receta_p.setVisible(true);
+				calorias_p.setVisible(false);
+				
+			}
+		});
 		recetas_b.setBounds(22, 60, 117, 29);
 		add(recetas_b);
 		
@@ -49,13 +68,22 @@ public class PanelGeneral extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				consejos_p.setVisible(true);
 				datos_p.setVisible(false);
-				
+				receta_p.setVisible(false);
+				calorias_p.setVisible(false);
 			}
 		});
 		consejos_b.setBounds(22, 101, 117, 29);
 		add(consejos_b);
 		
 		JButton calorias_b = new JButton("Calorias");
+		calorias_b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				consejos_p.setVisible(false);
+				datos_p.setVisible(false);
+				receta_p.setVisible(false);
+				calorias_p.setVisible(true);
+			}
+		});
 		calorias_b.setBounds(22, 142, 117, 29);
 		add(calorias_b);
 		
@@ -64,8 +92,10 @@ public class PanelGeneral extends JPanel {
 		JButton datos_b = new JButton("Datos");
 		datos_b.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				datos_p.setVisible(true);
 				consejos_p.setVisible(false);
+				datos_p.setVisible(true);
+				receta_p.setVisible(false);
+				calorias_p.setVisible(false);
 			}
 		});
 		datos_b.setBounds(22, 183, 117, 29);
